487021185857e9027e0bf40302c8f6de11b96a7d
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-20 14:54:07 -0500

    Update LinkedList.java
    Will worrk

--- LinkedList.java
+++ LinkedList.java
@@ -1,53 +1,70 @@
+
 /**
- * Add new data to end of list
- * Remove from beginning of list
- * 
- * @author pcmcknew1
  *
+ * @author User
  */
-public class LinkedList {
+public class LinkedList
+{ 
+   private Node first = null;
 
-	private Node head;
+    /**
+     *
+     */
+    public LinkedList()
+   {
+      //first = new Node(4);
+      first=null;
+   }
 
-	
-	/**
-	 * 
-	 */
-	public LinkedList() {
-		head = new Node(.3f);
-	}
-	/**
-	 * 
-	 * @param newFloat 
-	 * 
-	 */
-	public void addBegin(float newFloat){
-		Node nN=new Node(newFloat);
-		nN.setNext(head);
-		head=nN;
-	}
+    /**
+     *
+     * @return
+     */
+    public boolean isEmpty()
+   {
+      boolean e;
+      if(first==null){
+         e = true;
+      }
+      else{
+         e = false;
+      }
+      return e;
+   }
 
-	/**
-	 * @return If 
-	 */
-	public boolean isEmpty() {
-		boolean b;
-		if (head == null) {
-			b = true;
-		} else {
-			b = false;
-		}
-		return b;
-
-	}
-
-	@Override
-	public String toString() {
-		Node current = head;
-		while (current != null) {
-			current.getNext();
-			current = current.getNext();
-		}
-		return "LinkedList [head=" + current + ", isEmpty()=" + isEmpty() + "]";
-	}
+    /**
+     *
+     * @param e
+     */
+    public void add(float e){
+   
+      { Node newNode = new Node(e);     // create a new Node object
+      
+         //if (isEmpty())
+         //{ 
+         newNode.setNext(first);
+         first = newNode;
+         //}
+         // else 
+         // {// traverse the list to the end
+            // Node current = first;
+            // while (current.getNext() != null)
+            // {  current = current.getNext();
+            // }
+         //   //add newNode to the end
+            // current.setNext(newNode);
+         // }
+      }
+   }
+   public String toString()
+   {
+      String p="";
+      Node ref=first;
+      while(ref!=null)
+      {
+         p+=ref.getDataValue()+", ";
+         ref=ref.getNext();
+      }
+      return p;
+   }
 }
