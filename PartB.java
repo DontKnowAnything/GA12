7ba85996eed2cddda4da895931863e4958529805
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-24 08:53:28 -0500

    Update and rename PartB to PartB.java

--- PartB.java
+++ PartB.java
@@ -5,13 +5,19 @@
  */
 public class PartB {
 
-public static void main(String[] args) {
+   	    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
    	// TODO Auto-generated method stub
       LinkedList d=new LinkedList();
       d.add(4f);
+      d.add(5f);
       d.add(6f);
+      
+      d.remove();
       System.out.print(d.toString());
    
-   }
-
+   } 
 }
